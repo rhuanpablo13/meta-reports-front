12388d862753bae1179d670efbbb27b8f1e36462
rhuan <rhuanpablo13@hotmail.com>
2022-09-09 01:15:53 -0300

    criando entidades

---
+++ front/src/app/models/forecast.ts
@@ -0,0 +1,9 @@
+export interface Forecast {
+    id:number;
+    date:string;
+    weekday:string;
+    max:number;
+    min:number;
+    description:string;
+    condition:string;
+}
---
+++ front/src/app/models/weather.ts
@@ -0,0 +1,20 @@
+export interface Weather {
+
+    id:number;
+    conditionCode:string;
+    imgId:string;
+    date:string;
+    windSpeedy:string;
+    conditionSlug:string;
+    city:string;
+    cityName:string;
+    description:string;
+    currently:string;
+    cid:string;
+    sunrise:string;
+    sunset:string;
+    time:string;
+    temp:number;
+    humidity:number;
+
+}
