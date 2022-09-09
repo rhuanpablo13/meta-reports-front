ea09629f1f1f0a6c2551a5720f9706190de5c786
rhuan <rhuanpablo13@hotmail.com>
2022-09-09 02:12:26 -0300

    listando registros na tela

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { Weather } from './models/weather';
+import { WeatherService } from './services/weather.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'front';
+	title = 'front';
+
+	weather = {} as Weather;
+	weathers = {} as Weather[];
+
+	constructor(private weatherService: WeatherService) {}
+
+	ngOnInit() {
+		this.getWeathers();
+	}
+
+	getWeather() {
+		this.weatherService.getWeather().subscribe((weather: Weather) => {
+		  this.weather = weather;
+		});
+	}
+
+	getWeathers() {
+		this.weatherService.getWeathers().subscribe((weathers: Weather[]) => {
+		  this.weathers = weathers;
+		});
+	}
+
+	adicionarRegistroAleatorio() {
+		this.weatherService.adicionarRegistroAleatorio().subscribe((weather: Weather) => {
+		  this.weather = weather;
+		});
+		this.getWeathers();
+	}
 }
---
+++ front/src/app/services/weather.service.ts
@@ -0,0 +1,70 @@
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, retry } from 'rxjs/operators';
+import { Weather } from '../models/weather';
+
+
+@Injectable({
+	providedIn: 'root'
+})
+
+export class WeatherService {
+
+	URL = 'http://localhost:8080/api'
+
+
+	constructor(private httpClient: HttpClient) { }
+
+	// Headers
+	httpOptions = {
+		headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+	}
+
+	// Obtem todos os dados da api externa
+	getWeathers(): Observable<Weather[]> {
+		return this.httpClient.get<Weather[]>(this.URL + '/forecast/local')
+			.pipe(
+			retry(2),
+			catchError(this.handleError))
+	}
+
+	// Obtem todos os dados da api externa
+	getWeather(): Observable<Weather> {
+		return this.httpClient.get<Weather>(this.URL + '/forecast/api-externa/listar')
+			.pipe(
+			retry(2),
+			catchError(this.handleError))
+	}
+
+	// Obtem todos os dados da api externa
+	adicionarRegistroAleatorio(): Observable<Weather> {
+		return this.httpClient.get<Weather>(this.URL + '/forecast/api-externa/salvar')
+			.pipe(
+			retry(2),
+			catchError(this.handleError))
+	}
+
+	// salva um carro
+	// saveCar(weather: Weather): Observable<Weather> {
+	// 	return this.httpClient.post<Weather>(this.URL + 'forecast', JSON.stringify(weather), this.httpOptions)
+	// 	  .pipe(
+	// 		retry(2),
+	// 		catchError(this.handleError)
+	// 	)
+	// }
+
+	// Manipulação de erros
+	handleError(error: HttpErrorResponse) {
+		let errorMessage = '';
+		if (error.error instanceof ErrorEvent) {
+		  // Erro ocorreu no lado do client
+		  errorMessage = error.error.message;
+		} else {
+		  // Erro ocorreu no lado do servidor
+		  errorMessage = `Código do erro: ${error.status}, ` + `menssagem: ${error.message}`;
+		}
+		console.log(errorMessage);
+		return throwError(errorMessage);
+	};
+}
